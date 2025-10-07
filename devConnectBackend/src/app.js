2c904586752e3e7f6dc121baba8e649fd45432c0
keval.nagariya27 <kevalnagariya27@gmail.com>
2025-10-07 16:05:13 +0530

    wildcard error handling

--- devConnectBackend/src/app.js
+++ devConnectBackend/src/app.js
@@ -2,14 +2,28 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth");
+app.use("/", (err, req, res, next) =>{
+    if (err){
+        res.status(500).send("something went wrong");
+    }
+});
 
-app.use("/admin", adminAuth);
-// app.use("/user", userAuth);
 
-app.get("/user", userAuth, (req, res) =>{
+
+
+app.get("/getUserData", (req, res) =>{
+    // try{
+
+    throw new Error("asdasd");
     res.send("User Data sent");
+    // }
+    // catch(err){
+        // res.status(500).send("something went wrong");
+    // }
+
+    
 })
+
 
 app.get("/admin/getAllData", (req, res) =>{
     res.send("All data send");
