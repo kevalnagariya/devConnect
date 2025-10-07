3ca9e6c1a07e118139b52bf14c727d1dcfcb1b48
keval.nagariya27 <kevalnagariya27@gmail.com>
2025-10-07 15:00:19 +0530

    playing with multiple route handlers

--- devConnectBackend/src/app.js
+++ devConnectBackend/src/app.js
@@ -2,10 +2,28 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/route", rH, [rH2, rH3], rH4, rH5);
 
-app.get("/ab?c",(req, res) => {
-    res.send({firstName: "Keval", lastName:"nagariya"});
-})
+app.use("/user",(req, res, next) => {
+    console.log("handling the route user 1");
+    // res.send("Response");
+    next();
+},(req, res, next ) => {
+    console.log("handling the route user 2");
+    // res.send("2nd Response");
+    next();
+},(req, res, next) => {
+    console.log("handling the route user 4");
+    // res.send("3nd Response");
+    next();
+},(req, res, next) => {
+    console.log("handling the route user 4");
+    // res.send("4nd Response");
+    next();
+},(req, res) => {
+    console.log("handling the route user 5");
+    res.send("5nd Response");
+});
 
 
 app.listen(7777, ()=> {
