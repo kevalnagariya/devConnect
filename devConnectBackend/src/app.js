c65deaa3e93e10c2571b4bda3f65e4bb76e83e00
keval.nagariya27 <kevalnagariya27@gmail.com>
2025-10-07 13:46:22 +0530

    exploring get, post, delete

--- devConnectBackend/src/app.js
+++ devConnectBackend/src/app.js
@@ -2,10 +2,40 @@ const express = require("express");
 
 const app = express();
 
+//Order mattter
+// app.use("/user",(req, res) => {
+//     res.send("hahahahaahah")
+// })
+
+//This will only handle get call from the /user
+app.get("/user",(req, res) => {
+    res.send({firstName: "Keval", lastName:"nagariya"})
+})
+
+app.post("/user",(req, res) => {
+    //saving data to db
+    res.send("Data successfully saved to the database")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully")
+})
+
+// this will match all the http method api calls to /test
 app.use("/test",(req, res) => {
     res.send("Hello from the server")
 })
 
-app.listen(3000, ()=> {
+// this will match all the http method api calls to /hello
+app.use("/hello",(req, res) => {
+    res.send("Hello hello from the server")
+})
+
+// this will match all the http method api calls to /
+// app.use("/",(req, res) => {
+//     res.send("Hello from Keval")
+// })
+
+app.listen(7777, ()=> {
     console.log("Server is successfully listening on port 3000");
 });
