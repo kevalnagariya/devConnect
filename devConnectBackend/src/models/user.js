17715fde34902593a4c60aa0072e54b652117f50
keval.nagariya27 <kevalnagariya27@gmail.com>
2025-10-08 15:23:45 +0530

    exploring validator library

--- devConnectBackend/src/models/user.js
+++ devConnectBackend/src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -17,11 +17,21 @@ const userSchema = new mongoose.Schema(
       required: true,
       lowercase: true,
       unique: true,
-      trim: true
+      trim: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error("Invalid Email:" +value)
+        }
+      }
     },
     password: {
       type: String,
-      required: true
+      required: true,
+      validate(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error("Enter a strong password:" +value)
+        }
+      }
     },
     age: {
       type: Number,
@@ -37,7 +47,12 @@ const userSchema = new mongoose.Schema(
     },
     photoUrl: {
         type: String,
-        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        validate(value){
+        if(!validator.isURL(value)){
+            throw new Error("Invalid Photo Url:" +value)
+        }
+      }
     },
     about: {
         type: String,
