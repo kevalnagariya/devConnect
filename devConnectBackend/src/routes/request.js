e49835a378471b7b3ba9c07a73b77fe16f86dab7
keval.nagariya27 <kevalnagariya27@gmail.com>
2025-10-09 15:44:07 +0530

    queries and indexes in db & handled corner cases

--- devConnectBackend/src/routes/request.js
+++ devConnectBackend/src/routes/request.js
@@ -1,11 +1,66 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  console.log("Sending a connection request");
-  res.send(user.firstName + " Sent the connection request");
-});
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+
+      const existingConnecionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnecionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists" });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      const statusMessages = {
+        interested: `${req.user.firstName} is interested in ${toUser.firstName}`,
+        ignored: `${req.user.firstName} ignored ${toUser.firstName}`,
+      };
+
+      res.json({
+        message: statusMessages[status] || "Unknown status",
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("Error: " + err.message);
+    }
+  }
+);
 
 module.exports = requestRouter;
