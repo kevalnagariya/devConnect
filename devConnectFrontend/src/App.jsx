8043f6b79b3642e96849c472aa7730dd13e05b81
keval.nagariya27 <kevalnagariya27@gmail.com>
2025-10-14 19:33:52 +0530

    devconnect ui with its functionality

--- devConnectFrontend/src/App.jsx
+++ devConnectFrontend/src/App.jsx
@@ -1,28 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
-import Body from "./Body"
-import Login from "./Login"
-import Profile from "./Profile"
 
 function App() {
-
-
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-          </Route>
-
-        </Routes>
-
-      </BrowserRouter>
-     
-
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ devConnectFrontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-base-200 text-neutral-content p-4 bottom-0 fixed  ">
+      <aside>
+     
+        <p className="font-bold">
+          👩‍💻 DevConnect
+          <br />
+         
+        </p>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+      </aside>
+     
+    </footer>
+  );
+};
+
+export default Footer;
